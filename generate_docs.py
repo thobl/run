e3198b9329e7d10949ffd71218b82608118347c7
Thomas Bläsius <thomas.blaesius@kit.edu>
2021-03-24 17:01:47 +0100

    adjusted readme to refer to documentation pages

--- generate_docs.py
+++ generate_docs.py
@@ -6,12 +6,12 @@ import os
 
 here = Path(__file__).parent
 
-with open(here / "index.py", "w") as readme_py:
-    print("'''", file=readme_py)
-    with open(here / "README.md", "r") as readme_md:
-        readme = readme_md.read()
-        print(readme.replace("docs/", "./"), file=readme_py)
-    print("'''", file=readme_py)
+with open(here / "index.py", "w") as index_py:
+    print("'''", file=index_py)
+    with open(here / "INDEX.md", "r") as index_md:
+        index = index_md.read()
+        print(index.replace("docs/", "./"), file=index_py)
+    print("'''", file=index_py)
 
 render.configure(docformat="numpy",
                  template_directory=here / "pdoc-template")
